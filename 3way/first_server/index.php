6bc70abd3e3728296f69443ac83a7ec2fc732d1b
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-06-03 00:33:47 +0530

    updates

--- 3way/first_server/index.php
+++ 3way/first_server/index.php
@@ -3,25 +3,73 @@ $servername = "sql_server";
 $username = "root";
 $password = "your_password";
 $dbname = "your_database";
-$setName = array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8');
+$setName = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $setName);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Handle the text insertion request
+    $text = $_POST['text'];
 
-    // Fetch texts from the database
-    $sql = "SELECT text FROM your_table";
-    $stmt = $conn->query($sql);
-
-    if ($stmt->rowCount() > 0) {
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo $row["text"] . "<br>";
-        }
-    } else {
-        echo "No texts found.";
+    // Check if the text is empty
+    if (empty($text)) {
+        echo "Text is empty.";
+        return;
     }
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+
+    try {
+        // Create a connection
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $setName);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Insert the text into the database with suffix - from server 1 -
+        $sql = "INSERT INTO your_table (text) VALUES ('$text - from server 1')";
+        $conn->exec($sql);
+
+        // refresh page
+        header("Refresh:0");
+
+
+
+    } catch (PDOException $e) {
+        echo "Error occurred while inserting text: " . $e->getMessage();
+    }
+
+    $conn = null;
+
+} else {
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $setName);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Fetch texts from the database
+        $sql = "SELECT text FROM your_table";
+        $stmt = $conn->query($sql);
+
+        // create an HTML list of the texts
+        $html = "<ul>";
+
+        if ($stmt->rowCount() > 0) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $html .= "<li>" . $row['text'] . "</li>";
+            }
+
+            $html .= "</ul>";
+
+            // Also add a form to insert a new text which sends a POST request to this server
+            $html .= <<<HTML
+            <form method="post">
+                <input type="text" name="text" />
+                <input type="submit" value="Insert" />
+            </form>
+        HTML;
+
+            echo $html;
+        } else {
+            echo "No texts found.";
+        }
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 }
+
 ?>
--- 3way/second_server/index.php
+++ 3way/second_server/index.php
@@ -6,33 +6,65 @@ $dbname = "your_database";
 $firstServerUrl = "http://first_server";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle the text insertion request
-    $text = $_POST['text'];
+    try {
 
-    // Check if the text is empty
-    if (empty($text)) {
-        echo "Text is empty.";
-        return;
+        // Handle the text insertion request
+        $text = $_POST['text'];
+
+
+        // Check if the text is empty
+        if (empty($text)) {
+            echo "Text is empty.";
+            return;
+        }
+
+        // Create a connection
+        $conn = mysqli_init();
+        mysqli_real_connect($conn, $servername, $username, $password, $dbname);
+
+        // Insert the text into the database with suffix - from server 2 -
+        $sql = "INSERT INTO your_table (text) VALUES ('$text - from server 2')";
+        if (mysqli_query($conn, $sql)) {
+            // refresh page
+            header("Refresh:0");
+        } else {
+            echo "Error occurred while inserting text: " . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    } catch (Exception $e) {
+        echo "Error occurred while inserting text: " . $e->getMessage();
     }
-
-    // Create a connection
+} else {
+    // Fetch texts from the database
     $conn = mysqli_init();
+
     mysqli_real_connect($conn, $servername, $username, $password, $dbname);
 
-    // Insert the text into the database
-    $sql = "INSERT INTO your_table (text) VALUES ('$text')";
-    if (mysqli_query($conn, $sql)) {
-        echo "Text inserted successfully.";
-    } else {
-        echo "Error occurred while inserting text: " . mysqli_error($conn);
+    $sql = "SELECT text FROM your_table";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo "<ul>";
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li>" . $row['text'] . "</li>";
+        }
     }
 
-    mysqli_close($conn);
-} else {
-    // Make a request to the first server to get the texts
-    $texts = file_get_contents($firstServerUrl);
+    echo "</ul>";
 
-    echo $texts;
+    // Also add a form to insert a new text which sends a POST request to this server
+
+    echo <<<HTML
+    <form method="post">
+        <input type="text" name="text" />
+        <input type="submit" value="Insert text" />
+    </form>
+HTML;
+
+
 }
 
 ?>
